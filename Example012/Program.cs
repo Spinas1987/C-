dd630bcc3e7907f2a3a00ec34bc3d2934d6b50b0
Morteza <spinas.ir@yandex.ru>
2023-09-24 15:42:25 +0300

    Example 012

---
+++ Example012/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите положительное число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+    if (i % 2 == 0)
+        Console.WriteLine(i + ": ");
+  
+
+
+
