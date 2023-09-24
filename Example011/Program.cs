9c261f2f54315b2385ec2f617cbb4da8256cd1ba
Morteza <spinas.ir@yandex.ru>
2023-09-24 15:14:14 +0300

    Added example 011

---
+++ Example011/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+Console.WriteLine($"{number} является четным");
+
+}
+else 
+{
+    Console.WriteLine($"{number} является не четным");
+
+}
