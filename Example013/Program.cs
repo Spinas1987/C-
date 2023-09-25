8fa2301d3d124b7f19de1246f8a9ab27196c1ce4
Morteza <spinas.ir@yandex.ru>
2023-09-25 20:35:32 +0300

    Added Example 013

---
+++ Example013/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезки [10,99] -> {number}");
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+
+int maxDigit = MaxDigit (number);
+Console.WriteLine($"Наиьольшая цифра число: -> {maxDigit}");
