c234eea232f4190aa4ed9309b0072bf75ac2e622
Morteza <spinas.ir@yandex.ru>
2023-10-01 16:06:31 +0300

    Added Example 018

---
+++ Example018/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+int CountDigit(int num)
+{
+    if (num == 0) return 1;
+    int count = 0;
+    while (num != 0)    //!= yani num mokhalefe sefr
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = CountDigit(number);
+Console.WriteLine($"Колличество цифр в числе {result}");
